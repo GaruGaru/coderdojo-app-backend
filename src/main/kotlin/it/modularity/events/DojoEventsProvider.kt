9ad7b6f135e264032bcf45e072cc5a0a168c476d
GaruGaru <garuglieritommaso@gmail.com>
2018-01-13 17:58:32 +0100

    Removed time sorting in event provider to match api response sorting, updated docer-stack with traefik configuration

--- src/main/kotlin/it/modularity/events/DojoEventsProvider.kt
+++ src/main/kotlin/it/modularity/events/DojoEventsProvider.kt
@@ -9,9 +9,11 @@ import java.time.Instant
 class DojoEventsProvider(private val providers: List<DojoEventProvider>) {
     fun provide(query: String, latitude: Double, longitude: Double, range: Double): Single<List<DojoEvent>> =
             Observable.merge(providers.map { it.provide(query, latitude, longitude, range) })
-                    .sorted { o1, o2 -> o1.startTime.compareTo(o2.startTime) }
+                   // .sorted { o1, o2 -> o1.startTime.compareTo(o2.startTime) }
                     .filter({ it.endTime > now() })
                     .toList()
-}
 
-private fun now() = Instant.now().toEpochMilli()
+
+    private fun now() = Instant.now().toEpochMilli()
+
+}
